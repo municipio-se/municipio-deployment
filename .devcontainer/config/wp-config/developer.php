986a331ed1832a044fbb2715239bb3ad5255fd55
Thor Brink <thor.brink@helsingborg.se>
2025-03-17 12:05:22 +0000

    fix: update WordPress configuration to use dynamic protocol and host for URLs

--- .devcontainer/config/wp-config/developer.php
+++ .devcontainer/config/wp-config/developer.php
@@ -12,17 +12,20 @@
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 
+ $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+ $host = $_SERVER['HTTP_HOST'];
+
 // Activate debug mode on all environments using ?debug flag. 
 if (isset($_GET['debug'])) {
   define('WP_DEBUG', true);
 }
 
 if (!defined('WP_SITEURL')) {
-  define('WP_SITEURL', 'https://localhost:8443/wp');
+  define('WP_SITEURL', $protocol . $host . '/wp');
 }
 
 if (!defined('WP_HOME')) {
-  define('WP_HOME', 'https://localhost:8443');
+  define('WP_HOME', $protocol . $host);
 }
 
 if (!defined('WP_ENVIRONMENT_TYPE')) {
