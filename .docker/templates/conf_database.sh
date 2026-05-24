ca3575d44890ec16ae80ca3c1086ac5456a08a81
MrSpex <mail@dannilsson.se>
2026-05-24 11:18:07 +0200

    🚧wip(conf db): comment out SEARCH_REPLACE

--- .docker/templates/conf_database.sh
+++ .docker/templates/conf_database.sh
@@ -9,27 +9,27 @@ sed -i "s|(#db_password#)|${MYSQL_PASSWORD}|g" "config/database.php"
 sed -i "s|(#db_host#)|${MYSQL_HOST}|g" "config/database.php"
 sed -i "s|(#table_prefix#)|hbg_|g" "config/database.php"
 
-SEARCH_REPLACE_FLAG=".search_replace_done"
-if [ ! -f "$SEARCH_REPLACE_FLAG" ]; then
-  wp search-replace dev.local.municipio.tech "${PUBLIC_DOMAIN}" \
-    $(wp db tables --all-tables-with-prefix --format=csv | tr ',' '\n' | grep -Ev '^hbg_[0-9]+_') \
-    --skip-plugins \
-    --skip-themes \
-    --allow-root \
-    --url="${PUBLIC_DOMAIN}" \
-    --precise \
-    --report-changed-only \
-    --dry-run
-
-  wp search-replace https:// http:// \
-    $(wp db tables --all-tables-with-prefix --format=csv | tr ',' '\n' | grep -Ev '^hbg_[0-9]+_') \
-    --skip-plugins \
-    --skip-themes \
-    --allow-root \
-    --url="${PUBLIC_DOMAIN}" \
-    --precise \
-    --report-changed-only \
-    --dry-run
-
-  touch "$SEARCH_REPLACE_FLAG"
-fi
+#SEARCH_REPLACE_FLAG=".search_replace_done"
+#if [ ! -f "$SEARCH_REPLACE_FLAG" ]; then
+#  wp search-replace dev.local.municipio.tech "${PUBLIC_DOMAIN}" \
+#    $(wp db tables --all-tables-with-prefix --format=csv | tr ',' '\n' | grep -Ev '^hbg_[0-9]+_') \
+#    --skip-plugins \
+#    --skip-themes \
+#    --allow-root \
+#    --url="${PUBLIC_DOMAIN}" \
+#    --precise \
+#    --report-changed-only \
+#    --dry-run
+#
+#  wp search-replace https:// http:// \
+#    $(wp db tables --all-tables-with-prefix --format=csv | tr ',' '\n' | grep -Ev '^hbg_[0-9]+_') \
+#    --skip-plugins \
+#    --skip-themes \
+#    --allow-root \
+#    --url="${PUBLIC_DOMAIN}" \
+#    --precise \
+#    --report-changed-only \
+#    --dry-run
+#
+#  touch "$SEARCH_REPLACE_FLAG"
+#fi
