a114e5d7d508c36efdc973d4278fee889d687bc0
MrSpex <mail@dannilsson.se>
2026-05-24 10:37:22 +0200

    🔧conf(db): update table prefix

--- .docker/templates/conf_database.sh
+++ .docker/templates/conf_database.sh
@@ -7,7 +7,7 @@ sed -i "s|(#db_name#)|${MYSQL_DATABASE}|g" "config/database.php"
 sed -i "s|(#db_user#)|${MYSQL_USER}|g" "config/database.php"
 sed -i "s|(#db_password#)|${MYSQL_PASSWORD}|g" "config/database.php"
 sed -i "s|(#db_host#)|${MYSQL_HOST}|g" "config/database.php"
-sed -i "s|(#table_prefix#)|mun_|g" "config/database.php"
+sed -i "s|(#table_prefix#)|hbg_|g" "config/database.php"
 
 SEARCH_REPLACE_FLAG=".search_replace_done"
 if [ ! -f "$SEARCH_REPLACE_FLAG" ]; then
