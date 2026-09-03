481661170f6595ebe2fee7e1282de99106591283
MrSpex <mail@dannilsson.se>
2026-09-03 13:32:50 +0200

    🔧conf(wp): deactivate s3-uploads plugin
    Add s3-uploads to the network-wide plugin deactivation list in wp_deactivate_plugins.sh.

--- .docker/templates/wp_deactivate_plugins.sh
+++ .docker/templates/wp_deactivate_plugins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-wp plugin deactivate force-ssl active-directory-api-wp-integration \
+wp plugin deactivate s3-uploads force-ssl active-directory-api-wp-integration \
   --network \
   --allow-root \
   --url="${PUBLIC_DOMAIN}"
